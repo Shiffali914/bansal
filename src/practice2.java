c8737c19044aa1ea2d4984db1ae3e6c737f565b2
Shiffali914 <Shiffalibansal914@gmail.com>
2024-12-19 16:47:00 -0500

    new correction

--- src/practice2.java
+++ src/practice2.java
@@ -2,7 +2,6 @@ public class practice2 {
     public static void main(String[] args) {
         int x=10;
         int y=20;
-
         boolean result =x<y;
         boolean result1= x>y;
         boolean result2= !(x==y); //10==20 ----!(false) --true
